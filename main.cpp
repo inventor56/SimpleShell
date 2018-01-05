1102b725532b7696abd9116a6cca758b6c8bab7e
Joshua Smith Laptop <joshner.smith@gmail.com>
2018-01-04 22:42:48 -0800

    - Finished first rough version

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
 #include <unistd.h>
-#include <cstdio>
-#include <cstring>
+#include <vector>
 #include <sys/wait.h>
+#include <sstream>
+#include <iterator>
+#include <cstring>
 
 using namespace std;
 
 int main() {
 
     while(true) {
-
-        string command; // Full initial user input (i.e. cd /Documents/SchoolBudget)
+        // Full initial user input (i.e. cd /Documents/SchoolBudget)
+        string userInput;
         // Begin by prompting the user for input
-        cout << "Hello! Welcome to the Shell. Please enter in your Linux command: " << endl;
+        cout << "Hello! Welcome to the Shell. Please enter in your Linux arguments: " << endl;
         //Store input;
-        cin >> command; // The entire command to execute in the shell, including arguments
-
+        cin >> userInput; // The entire user input
         // Terminate the program if exit is the input
-        if (command == "exit") {
+        if (userInput == "exit") {
             exit(0); // Exit with EXIT_SUCCESS returned to the host environment
         }
 
-        // Otherwise, parse out input
-        char *filename = const_cast<char *>("/bin/pwd"); // Be sure you can navigate to this directory
-        char *arguments[2];
-        arguments[0] = const_cast<char *>("pwd");
-        arguments[1] = nullptr;
+        // Otherwise, parse user input (split into words delimited by whitespace
+        auto * filename = const_cast<char *>("/bin/"); // First element refers to path of executable file
+        vector<char *> arguments;
+
+        istringstream inputStream(userInput); // Create input stream out of the user input
+        vector<string> items{istream_iterator<string>{inputStream}, istream_iterator<string>{}}; // Iterate through input stream and create new String vector with each individual word
+
+        strcat(filename, items.front()); // Concatenate the filename (i.e. "/bin/") with what the user's first input was (i.e. "pwd")
+
+        for (auto const& item: items) {
+            arguments.push_back(const_cast<char *&&>(item.c_str())); // Place each argument into your char *arguments vector
+        }
+        arguments.push_back(nullptr); // Finally, the last portion of the vector will be a null pointer (a NULL terminator to tell it to end)
 
         //const char *test = "pwd";
         //strcat(filename, "pwd");
@@ -44,7 +53,7 @@ int main() {
         if (pid == 0) { // Child process
             try {
                 cout << "we made it inside the child process" << endl;
-                execvp(filename, arguments); // "useful when the number  of  arguments is unknown in advance" (Marshall). We pass file name and arguments
+                execvp(filename, arguments.data()); // We pass file name and arguments (filename will be, for instance, /bin/ls)
 
             }
             catch (exception e) {
